9d560ac785496f2ebdb9c52a46924b68617963fd
Mark Locklear <marklocklear@gmail.com>
2021-04-09 09:47:09 -0400

    added basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'welcome', to: 'sessions#welcome', as: 'welcome'
+
+  resources :users
+
+  root to: "sessions#welcome"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
