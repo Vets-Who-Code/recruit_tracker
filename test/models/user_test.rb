50fa768aa5cf3efc703773540c6017bee13a1466
Mark Locklear <marklocklear@gmail.com>
2021-04-15 09:57:29 -0400

    intial set up for tests along with first basic test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+	test "should not save user without email" do
+		user = User.new
+		assert_not user.save
+	end
+
+	test "should save user with email" do
+		user = User.new
+		user.email = "mark@blarg.com"
+		user.password = "password"
+		assert user.save
+	end
   # test "the truth" do
   #   assert true
   # end
