e984468743a3abe8c3c6830c46cf5f9cf6814afa
Mark Locklear <marklocklear@gmail.com>
2021-04-19 10:39:33 -0400

    add edit and update user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update ]
+
+  def index
+    @users = User.all
+  end
 
   def show
 
@@ -8,6 +13,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,6 +24,18 @@ class UsersController < ApplicationController
       redirect_to root_url, notice: "Thank you for signing up!"
     else
       render "new"
+    end
+  end
+
+   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "user was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
