82bec29f61371e41dcfb68ec9fd298a43389f668
Greg Curl <greg.a.curl@gmail.com>
2021-04-20 07:59:26 -0400

    Adding flash message to login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to welcome_path, notice: "Logged in!"
     else
       flash.now.notice = "Email or password is invalid"
       render "new"
