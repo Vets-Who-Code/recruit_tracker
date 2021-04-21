6b56d7ae1d348ae70af8d35ed13d89a5ed1efe68
Greg Curl <greg.a.curl@gmail.com>
2021-04-21 12:04:59 -0400

    Add Auth to users and fields to sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def show
-    
-  end
-  
+  #validations section
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  # validates :password, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
+  #   message: "must include at least one lowercase letter, one uppercase letter, and one digit" }
+
 end
