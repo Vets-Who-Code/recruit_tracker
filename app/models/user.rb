bfe36eaeacac1c7f8e1002ce64b214cf5927f59e
Mark Locklear <marklocklear@gmail.com>
2021-05-06 12:04:47 -0400

    added password complexity

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   #validations section
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  # validates :password, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
-  #   message: "must include at least one lowercase letter, one uppercase letter, and one digit" }
+  validates :password, format: { with: /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
+    message: "must include at least one lowercase letter, one uppercase letter, and one digit" }
 
 	# User roles will correspond to user types: admin, student, mentor, ect. 0 is most restrictive (student);
 	# 5 is most powerful (admins)
