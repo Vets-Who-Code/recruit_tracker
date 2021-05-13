597b5943f365cc6e3dfc286dabfe4b3588e9f82a
Mark Locklear <marklocklear@gmail.com>
2021-05-13 08:14:42 -0400

    update db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_04_182422) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cohorts", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
